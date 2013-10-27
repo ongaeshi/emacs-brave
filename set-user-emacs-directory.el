48267b94e77195764adefc5938fda9940dd0ddec
ongaeshi <ongaeshi0621@gmail.com>
2013-10-28 01:24:22 +0900

    Rename start-relative-path.rb -> set-user-emacs-directory.el

--- set-user-emacs-directory.el
+++ set-user-emacs-directory.el
@@ -1,5 +1,5 @@
 ;; How to use:
 ;;   $ cd /path/to/emacs-brave
-;;   $ /Applications/Emacs.app/Contents/MacOS/Emacs -Q -l start-relative-path.el -l init.el
+;;   $ /Applications/Emacs.app/Contents/MacOS/Emacs -Q -l set-user-emacs-directory.el -l init.el
 
 (setq user-emacs-directory (expand-file-name "."))
