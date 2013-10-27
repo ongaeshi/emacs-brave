5654c3e0ccc9b834c6cbb0266af8bf638cabc488
ongaeshi <ongaeshi0621@gmail.com>
2013-10-27 12:48:09 +0900

    Add comment

--- start-relative-path.el
+++ start-relative-path.el
@@ -1,1 +1,5 @@
+;; How to use:
+;;   $ cd /path/to/emacs-brave
+;;   $ /Applications/Emacs.app/Contents/MacOS/Emacs -Q -l start-relative-path.el -l init.el
+
 (setq user-emacs-directory (expand-file-name "."))
